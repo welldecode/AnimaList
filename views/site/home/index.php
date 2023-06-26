95695bca616e328db39c2446c966b2c761148003
welldecode <wellingtonalfredo550@gmail.com>
2023-06-26 15:40:42 -0300

    primeiro commit

--- views/site/home/index.php
+++ views/site/home/index.php
@@ -11,6 +11,23 @@
             <h1>Trending Now</h1>
             <h2>see the latest trends released</h2>
         </div>
- 
+
+        <?php
+                $i = 0;
+                foreach ($this->trending_now as $anime) : 
+                ?>
+                    <article class="splide__slide anime_i" data-aos="fade-up" data-aos-delay="<?php echo $i ?>00">
+                        <a href="toon/<?php echo $anime['info']['name'] ?>">
+                            <figure class="thumb">
+                             </figure>
+                            <h1 class="text"><?php echo $anime['info']['name'] ?></h1>
+                            <div class="flex justify-between align-center">
+                                <h2>Original</h2>
+                                <h3>Dub | Leg</h3>
+                            </div>
+                        </a>
+                    </article>
+                <?php $i++;
+                endforeach ?>
     </section>
 </main>
