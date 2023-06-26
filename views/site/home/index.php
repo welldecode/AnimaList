2bee57834709468eba29ce8683f3d7ea01e02110
welldecode <wellingtonalfredo550@gmail.com>
2023-06-26 13:47:03 -0300

    adicionando layout

--- views/site/home/index.php
+++ views/site/home/index.php
@@ -1,1 +1,15 @@
-Anima List
+<main>
+    <section class="top container">
+        <div class="top_content">
+            <h1>A nova plataforma de anime da próxima geração</h1>
+            <span>Acompanhe, compartilhe e descubra seus animes e mangás favoritos com o AnimaList.</span>
+        </div>
+    </section>
+
+    <section class="trending container">
+        <div class="titles">
+            <h1>Trending Now</h1>
+            <h2>see the latest trends released</h2>
+        </div>
+    </section>
+</main>
--- views/site/layout.php
+++ views/site/layout.php
@@ -1,16 +1,27 @@
 <!DOCTYPE html>
-<html lang="pt-br"> 
+<html lang="pt-br">
+
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">  
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title><?php echo $this->getTitlePage(); ?></title>
   <link rel="stylesheet" href="public/css/style.css">
 </head>
 
 <body>
-   
+  <header>
+    <div class="container">
+      <nav>
+        <h1>AnimaList</h1>
+        <ul>
+          <li><a href="#">Inicio</a></li>
+        </ul>
+      </nav>
+    </div>
+  </header>
   <?php echo $this->viewContent(); ?>
 
-</body> 
+</body>
+
 </html>
